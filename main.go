c693b44024030a92ade3ab91649a243ff71daa72
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-09-09 17:09:31 +0530

    main

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"log"
+
+	"github.com/harhvardha/peer-to-peer-distributed-storage/p2p"
+)
+
+func main() {
+	tr := p2p.NewTCPTransport(":3000")
+	if err := tr.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
+	select {}
+}
