f3ade7826e1dfd7479c47580b21f92431bbf5c63
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-09-10 23:43:51 +0530

    added Consume method to Transport interface

--- p2p/transport.go
+++ p2p/transport.go
@@ -2,10 +2,12 @@ package p2p
 
 // This interface represents a remote node
 type Peer interface {
+	Close() error
 }
 
 // Transport interface handles the communation between the nodes in the network
 // It can be of type TCP, UPD, WEBSOCKETS, ...
 type Transport interface {
 	ListenAndAccept() error
+	Consume() <-chan RPC
 }
