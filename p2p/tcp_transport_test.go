5af250565600b8513332abba7b603b66b7693cdf
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-09-10 23:44:58 +0530

    tests working properly

--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -7,9 +7,13 @@ import (
 )
 
 func TestTCPTransport(t *testing.T) {
-	listenAddr := ":4000"
-	tr := NewTCPTransport(listenAddr)
-	assert.Equal(t, tr.listenAddress, listenAddr)
+	ops := TCPTransportOps{
+		ListenAddr:    ":3000",
+		HandshakeFunc: NOPHandshakeFunc,
+		Decoder:       DefaultDecoder{},
+	}
+	tr := NewTCPTransport(ops)
+	assert.Equal(t, tr.ListenAddr, ":3000")
 
 	assert.Nil(t, tr.ListenAndAccept())
 }
