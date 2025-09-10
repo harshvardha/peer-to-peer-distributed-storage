0214b145b6d483fa5e051dbbd616e0560b1e1a86
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-09-10 23:46:38 +0530

    tcpOps initialization changed and launching a go routine to log messages getting consumed

--- main.go
+++ main.go
@@ -1,20 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/harhvardha/peer-to-peer-distributed-storage/p2p"
 )
+
+func OnPeer(peer p2p.Peer) error {
+	peer.Close()
+	//fmt.Println("doing some logic with peer outside of TCPTransport")
+	return nil
+}
 
 func main() {
 	tcpOps := p2p.TCPTransportOps{
 		ListenAddr:    ":3000",
 		Decoder:       p2p.DefaultDecoder{},
 		HandshakeFunc: p2p.NOPHandshakeFunc,
+		OnPeer:        OnPeer,
 	}
 
 	tr := p2p.NewTCPTransport(tcpOps)
+
+	go func() {
+		for {
+			msg := <-tr.Consume()
+			fmt.Printf("%+v\n", msg)
+		}
+	}()
+
 	if err := tr.ListenAndAccept(); err != nil {
 		log.Fatal(err)
 	}
+
+	select {}
 }
