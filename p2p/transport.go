9f2236480a4674216fb8381e2fa197942e81d8fa
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-09-11 18:11:55 +0530

    Peer interface has new Send method and embeds net.Conn interface and Transport interface has Dial an Close method now

--- p2p/transport.go
+++ p2p/transport.go
@@ -1,13 +1,18 @@
 package p2p
 
+import "net"
+
 // This interface represents a remote node
 type Peer interface {
-	Close() error
+	net.Conn
+	Send([]byte) error
 }
 
 // Transport interface handles the communation between the nodes in the network
 // It can be of type TCP, UPD, WEBSOCKETS, ...
 type Transport interface {
+	Dial(string) error
 	ListenAndAccept() error
 	Consume() <-chan RPC
+	Close() error
 }
